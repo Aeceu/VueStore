cd206f0f423ce739c2d785d0b4fccb473863485a
Aeceu <joseacebuche2@gmail.com>
2025-06-12 15:34:19 +0800

    added search input

---
+++ src/components/NavbarView.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="w-full h-20 flex items-center gap-8 px-8">
+    <h1 class="text-emerald-500 font-extrabold text-xl">TheDailyFind</h1>
+    <div>
+      <input v-model="search" placeholder="Search an item" class="border px-3 py-1 rounded-full" />
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const search = ref<string>('')
+</script>
+
+<style scoped></style>
